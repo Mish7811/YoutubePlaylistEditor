2d3eeb033f83f3827ddb45a0fb04ef3ca82774a4
Mish <164550454+Mish7811@users.noreply.github.com>
2025-04-02 12:16:59 +0530

    Update next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/api/playlist",
+        destination: "https://youtubeplaylisteditor-production.up.railway.app/playlist",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
